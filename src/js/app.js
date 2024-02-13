a9b7ce670d654ff62a37c7611f69e821407d835f
Samuel Frias Nieto <sae.nieto@gmail.com>
2024-02-13 17:33:08 +0100

    Parallax Effect

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,20 @@ document.addEventListener('DOMContentLoaded', function() {
     eventListeners();
 
     darkMode();
+
+    parallaxImage();
 })
+
+function parallaxImage() {
+    window.addEventListener('scroll', () => {
+        let pantalla = window.scrollY
+        let imagen = document.getElementById('imagen')
+        if(pantalla > 1136) {
+            let parallax = Math.min(pantalla*0.08888-100,100)
+            imagen.style.backgroundPosition = `center ${parallax}%`
+        }
+    })
+}
 
 function darkMode() {
     const darkButton = document.querySelector('.dark-mode')
