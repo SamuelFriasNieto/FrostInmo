22182d1f685bcf8881571bce06b60a9fbbeaca3e
Samuel Frias Nieto <sae.nieto@gmail.com>
2024-02-26 20:18:55 +0100

    Conexión e inserción en la base de datos

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -1,6 +1,41 @@
 <?php
-  require '../../includes/funciones.php';
-  incluirTemplate('header');
+        
+    require '../../includes/config/database.php';
+
+    $db = conectarDB();
+    $db->set_charset('utf8');
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    
+
+        $titulo = $_REQUEST['titulo'];
+        $precio = $_REQUEST['precio'];
+        $descripcion = $_REQUEST['descripcion'];
+        $habitaciones = $_REQUEST['habitaciones'];
+        $wc = $_REQUEST['wc'];
+        $estacionamiento = $_REQUEST['estacionamiento'];
+        $vendedor = $_REQUEST['vendedor'];
+
+        $query = " INSERT INTO propiedades (titulo, precio, descripcion, habitaciones, wc, estacionamiento, vendedores_id ) VALUES ( '$titulo', '$precio', '$descripcion', '$habitaciones', '$wc', '$estacionamiento', '$vendedor' ) ";
+
+        $resultado = mysqli_query($db,$query);
+        $error = mysqli_error($db);
+
+
+
+        echo $query;
+        echo $error;
+
+        if($resultado) {
+            echo 'insertado correctamente';
+        } else {
+            echo 'Ha pasado algo';
+        }
+    }
+
+    require '../../includes/funciones.php';
+    incluirTemplate('header');
 ?>
 
     <main class="contenedor seccion">
@@ -9,40 +44,40 @@
 
         <a href="/frostinmo/admin" class="boton boton-verde">Volver</a>
 
-        <form action="" class="formulario">
+        <form action="/frostinmo/admin/propiedades/crear.php" method="POST" class="formulario">
             <fieldset>
                 <legend>Información General</legend>
 
                 <label for="titulo">Título:</label>
-                <input type="text" name="" id="titulo" placeholder="Propiedad">
+                <input type="text" name="titulo" id="titulo" placeholder="Propiedad">
 
                 <label for="precio">Precio:</label>
-                <input type="text" name="" id="precio" placeholder="Precio Propiedad">
+                <input type="text" name="precio" id="precio" placeholder="Precio Propiedad">
 
                 <label for="imagen">Imágen:</label>
-                <input type="file" name="" id="imagen" accept="image/jpeg, image/png" placeholder="Imágen Propiedad">
+                <input type="file" name="imagen" id="imagen" accept="image/jpeg, image/png" placeholder="Imágen Propiedad">
 
                 <label for="descripcion">Descripción:</label>
-                <textarea name="" id="descripcion"></textarea>
+                <textarea name="descripcion" id="descripcion"></textarea>
             </fieldset>
 
             <fieldset>
                 <legend>Información de la Propiedad</legend>
 
                 <label for="habitaciones">Habitaciones:</label>
-                <input type="number" name="" id="habitaciones" placeholder="Ej: 3" min="1" max="9" >
+                <input type="number" name="habitaciones" id="habitaciones" placeholder="Ej: 3" min="1" max="9" >
 
                 <label for="wc">Baños:</label>
-                <input type="number" name="" id="wc" placeholder="Ej: 3" min="1" max="9" >
+                <input type="number" name="wc" id="wc" placeholder="Ej: 3" min="1" max="9" >
 
                 <label for="estacionamiento">Estacionamiento:</label>
-                <input type="number" name="" id="estacionamiento" placeholder="Ej: 3" min="1" max="9" >
+                <input type="number" name="estacionamiento" id="estacionamiento" placeholder="Ej: 3" min="1" max="9" >
             </fieldset>
 
             <fieldset>
                 <legend>Vendedor</legend>
 
-                <select name="" id="">
+                <select name="vendedor" id="">
                     <option value="1">Samuel</option>
                     <option value="2">Helena</option>
                 </select>
