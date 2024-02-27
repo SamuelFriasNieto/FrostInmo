39c81d0edaae10840ccd63f2277f9bd242b48d16
Samuel Frias Nieto <sae.nieto@gmail.com>
2024-02-27 12:02:23 +0100

    Imprimo las viviendas de la BD en la web

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,11 @@
 <?php
+  require '../includes/config/database.php';
+  $db = conectarDB();
+
+  $query = "SELECT * FROM propiedades";
+
+  $propiedades = mysqli_query($db,$query);
+
   require '../includes/funciones.php';
   incluirTemplate('header');
 
@@ -12,8 +19,37 @@
         <?php endif ?>
 
         <a href="/frostinmo/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
+
+        <table class="propiedades">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Título</th>
+              <th>Imagen</th>
+              <th>Precio</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <?php while($propiedad = mysqli_fetch_assoc($propiedades)): ?>
+            <tr>
+              <td><?= $propiedad['id'] ?></td>
+              <td><?= $propiedad['titulo'] ?></td>
+              <td><img src="/frostinmo/imagenes/<?= $propiedad['imagen'] ?>" class="imagen-tabla" alt=""></td>
+              <td>$<?= $propiedad['precio'] ?></td>
+              <td>
+                <a href="#" class="boton boton-rojo-block">Eliminar</a>
+                <a href="#" class="boton boton-azul-block">Actualizar</a>
+              </td>
+            </tr>
+            <?php endwhile; ?>
+          </tbody>
+        </table>
     </main>
 
-    <?php
+<?php
+  mysqli_close($db);
+
   incluirTemplate('footer');
 ?>
